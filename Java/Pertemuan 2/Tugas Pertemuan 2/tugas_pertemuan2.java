26594b070e26b707c1ca32ffa9341e740492d277
Ronaldi Chandra <ronhyun@protonmail.com>
2020-09-22 18:52:15 +0700

    Update tugas_pertemuan2.java

--- Java/Pertemuan 2/Tugas Pertemuan 2/tugas_pertemuan2.java
+++ Java/Pertemuan 2/Tugas Pertemuan 2/tugas_pertemuan2.java
@@ -2,9 +2,11 @@ public class tugas_pertemuan2{
     public static void main(String[] args) {
         
         int v1 = 10, v2 = 5, v3 = 2, hasil1, hasil2, hasil3;
+        
         hasil1 = v1 + v2 + v3;
         hasil2 = v1 - v3;
         hasil3 = v2 * v3;
+        
         System.out.println("V1 + V2 + V3 = " + hasil1);
         System.out.println("V1 - V3 = " + hasil2);
         System.out.println("V2 * V3 = " + hasil3);
