c15a9983572bf8e9a4f498ef48cafd059fed1385
Ronaldi Chandra <ronhyun@protonmail.com>
2020-09-24 18:03:17 +0700

    Create cara_clearscreen_di_java.java

---
+++ Java/cara_clearscreen_di_java.java
@@ -0,0 +1,13 @@
+public class cara_clearscreen_di_java {
+
+    public static void clrscr() {  
+      System.out.print("\033[H\033[2J");  
+      System.out.flush();  
+    }
+    
+    public static void main(String[] args) {
+      clrscr();
+      System.out.println("Follow @renebaebae");
+      
+  }
+}
