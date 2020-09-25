075436a717cba48402db7f4217578d60e2bdd3c0
Ronaldi Chandra <ronhyun@protonmail.com>
2020-09-25 12:27:37 +0700

    Create operator_rasional.java

---
+++ Java/Pertemuan 3/operator_rasional.java
@@ -0,0 +1,27 @@
+public class operator_rasional {
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 5;
+        boolean hasil;
+        
+        hasil = a == b;
+        System.out.println("Apakah a == b? " + hasil );
+        
+        hasil = a != b;
+        System.out.println("Apakah a != b? " + hasil );
+        
+        hasil = a > b;
+        System.out.println("Apakah a > b? " + hasil );
+        
+        hasil = a < b;
+        System.out.println("Apakah a < b? " + hasil );
+        
+        hasil = a >= b;
+        System.out.println("Apakah a >= b? " + hasil );
+        
+        hasil = a <= b;
+        System.out.println("Apakah a <= b? " + hasil );
+        
+        }
+}
