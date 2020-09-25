13f513c4b1e7bd7ed22a9f9b91b684f337189a3e
Ronaldi Chandra <ronhyun@protonmail.com>
2020-09-25 11:26:50 +0700

    tugas lagi tugas lagi

---
+++ Java/Pertemuan 3/Tugas/password.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class password {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        String username, password;
+        boolean hasil = true;
+        
+        System.out.print("Masukkan username: ");
+        username = inp.next();
+        System.out.print("Masukkan password: ");
+        password = inp.next();
+
+        if(password == "123456"){
+            hasil = true;
+        }
+
+        System.out.print("Password yang anda input? " + hasil);
+    }
+}
