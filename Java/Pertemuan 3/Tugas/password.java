05dcd1a1aba8df8f91730ef59dae77a9cdcce7e1
Ronaldi Chandra <ronhyun@protonmail.com>
2020-09-25 11:33:30 +0700

    crot

--- Java/Pertemuan 3/Tugas/password.java
+++ Java/Pertemuan 3/Tugas/password.java
@@ -3,18 +3,21 @@ import java.util.Scanner;
 public class password {
     public static void main(String[] args) {
         Scanner inp = new Scanner(System.in);
-        String username, password;
-        boolean hasil = true;
+        String username;
+        int password;
+        boolean hasil;
         
         System.out.print("Masukkan username: ");
         username = inp.next();
         System.out.print("Masukkan password: ");
-        password = inp.next();
+        password = inp.nextInt();
 
-        if(password == "123456"){
+        if(password == 123456){
             hasil = true;
+            System.out.print("Password yang anda input? " + hasil);
+        } else if(password != 123456){
+            hasil = false;
+            System.out.print("Password yang anda input? " + hasil);
         }
-
-        System.out.print("Password yang anda input? " + hasil);
     }
 }
