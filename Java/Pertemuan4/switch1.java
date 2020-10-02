493d6aa91abd483157afae76ec0338710def0c81
Ronaldi Chandra <ronhyun@protonmail.com>
2020-10-02 21:57:33 +0700

    Add files via upload

--- Java/Pertemuan4/switch1.java
+++ Java/Pertemuan4/switch1.java
@@ -2,11 +2,17 @@ package Pertemuan4;
 
 public class switch1 {
     public static void main(String[] args) {
-        int x=100;
+        int x=1;
         switch(x){
-            case 100:
-            System.out.println("ada");
-            break;
+            case 1:
+                System.out.println("X = 1");
+                break;
+            case 2:
+                System.out.println("X = 2");
+                break;
+            case 3:
+                System.out.println("X = 3");
+                break;
         }
     }
 }
