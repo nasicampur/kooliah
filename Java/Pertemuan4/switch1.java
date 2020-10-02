0e11e2254f7b9e14ad424f769df760ab4cd1956b
Ronaldi Chandra <ronhyun@protonmail.com>
2020-10-02 21:43:39 +0700

    Add files via upload

---
+++ Java/Pertemuan4/switch1.java
@@ -0,0 +1,12 @@
+package Pertemuan4;
+
+public class switch1 {
+    public static void main(String[] args) {
+        int x=100;
+        switch(x){
+            case 100:
+            System.out.println("ada");
+            break;
+        }
+    }
+}
