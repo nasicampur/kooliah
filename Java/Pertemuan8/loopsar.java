6d4b746f6cc43569538b1afaad6704cd680e1587
Ronaldi Chandra <ronhyun@protonmail.com>
2020-11-21 11:39:02 +0700

    Add files via upload

---
+++ Java/Pertemuan8/loopsar.java
@@ -0,0 +1,13 @@
+package Pertemuan8;
+
+public class loopsar {
+    public static void main(String[] args) {
+		int x, y;
+		for(x=1;x<=5;x++){
+	        for(y=1;y<=x;y++){
+	            System.out.print("*");
+	        }
+	        System.out.print("\n");
+	    }
+	}
+}
---
+++ Java/Pertemuan9/aw.java
@@ -0,0 +1,11 @@
+package Pertemuan9;
+import java.applet.*;
+import java.awt.*;
+
+public class aw extends Applet{
+
+	public void paint(Graphics g){
+		g.drawString("This Java Applet STMIK Dharma Putra",150,150);
+		}
+
+}
