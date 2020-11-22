2aa3dfa6dbd2fbd6e3ee32be61c7428f8018bf64
Ronaldi Chandra <ronhyun@protonmail.com>
2020-11-22 20:45:14 +0700

    Update biodata.py

--- Python/Latihan/biodata.py
+++ Python/Latihan/biodata.py
@@ -1,4 +1,16 @@
+from os import system, name
+def clear(): 
+    if name == 'nt': 
+        _ = system('cls') 
+  
+    else: 
+        _ = system('clear') 
+
 nama_depan = input("Masukkan nama depan: ")
 nama_belakang = input("Masukkan nama belakang: ")
-
+umur = input("Umur: ")
+alamat = input("Alamat: ")
+clear()
 print("Nama anda:", nama_depan + " " + nama_belakang)
+print("Umur anda:", umur)
+print("Alamat anda:", alamat)
